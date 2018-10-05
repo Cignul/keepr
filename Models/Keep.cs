4125444e5304770b54710665dfbf9f07199552e5
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-10-05 14:58:34 -0600

    working on keep.cs model, got constructor but need helper methods

---
+++ Models/Keep.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+namespace keepr.Models
+{
+
+  public class Keep // CONSTRUCTOR need helper methods to create, add, delete
+  {
+    [Required]
+    [MinLength(2)]
+    public string Name { get; set; }
+    [Required]
+    [MinLength(5)]
+    public string Description { get; set; }
+
+  }
+
+
+
+}
