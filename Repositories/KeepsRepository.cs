767b5906fd0748f8895640f08e40c3ffd12021fa
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-10-08 15:26:13 -0600

    started on keepsrepo

---
+++ Repositories/KeepsRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Data;
+using System.Linq;
+using BCrypt.Net;
+using keepr.Models;
+using Dapper;
+
+namespace keepr.Repositories
+{
+  public class KeepsRepository
+  {
+
+
+
+  }
+}
--- wwwsrc/src/views/Home.vue
+++ wwwsrc/src/views/Home.vue
@@ -5,10 +5,12 @@
     <form>form for creating keeps
       <input type="text" Default="name">
       <input type="text" Default="description">
+      <input type="submit">
     </form>
     <form>form for creating vaults
       <input type="text" Default="name">
       <input type="text" Default="description">
+      <input type="submit">
     </form>
     <ul>spot for keeps</ul>
     <!-- need to add logic to logout and @click -->
