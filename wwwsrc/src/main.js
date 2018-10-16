3458228ce02f6ab48bdcfcb42b05993b5576c978
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-10-16 16:16:42 -0600

    getting rid of vuetify to fix things

--- wwwsrc/src/main.js
+++ wwwsrc/src/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 // @ts-ignore
 import App from './App.vue'
-import Vuetify from 'vuetify'
 import router from './router'
 import store from './store'
-import 'vuetify/dist/vuetify.min.css'
 
 
 Vue.config.productionTip = false
